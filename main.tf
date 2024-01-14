f169921c5d62d4dd268ad909aa7bc298a63e469f
Takashi Takeyama <t757f37a@gmail.com>
2024-01-14 11:41:20 +0900

    End of Chapter 2

--- main.tf
+++ main.tf
@@ -2,25 +2,6 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# resource "aws_instance" "example" {
-# ami = "ami-0506f0f56e3a057a4"
-# ami = "ami-07c589821f2b353aa"
-# instance_type  = "t2.micro"
-# vpc_security_group_ids = [aws_security_group.instance.id]
-# 
-# user_data = <<-EOF
-# !/bin/bash
-# echo "Hello, World" > index.html
-# nohup busybox httpd -f -p 8080 &
-# EOF
-# 
-# user_data_replace_on_change = true
-# 
-# tags = {
-# Name = "terraform-example"
-# }
-# }
-
 resource "aws_security_group" "instance" {
   name = "terraform-exaple-instance"
 
@@ -36,8 +17,25 @@ resource "aws_security_group" "instance" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["193.186.4.152/32"]
-    # cidr_blocks = ["${aws_instance.example.public_ip/32}"]
   }
+}
+
+resource "aws_security_group" "alb" {
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  
 }
 
 resource "aws_launch_configuration" "example" {
@@ -61,6 +59,8 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnets.default.ids
 
+  target_group_arns = [ aws_lb_target_group.asg.arn ]
+
   min_size = 2
   max_size = 10
 
@@ -72,12 +72,64 @@ resource "aws_autoscaling_group" "example" {
 
 }
 
-# output "public_ip" {
-#   value       = aws_autoscaling_group.example.public_ip
-  # value       = aws_instance.example.public_ip
-  # description = "The public IP address of the webserver"
-  # sensitive   = true
-# }
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [ aws_security_group.alb.id ]
+}
+
+#リスナーはLBが待ち受けている設定 ターゲットグループへ転送
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP" 
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = [ "*" ]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    #ヘルスチェクの応答が健康とみなされる基準
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    #連続何回か？
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
@@ -96,4 +148,9 @@ data "aws_subnets" "default" {
     values = [ data.aws_vpc.default.id ]
   }
   
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
